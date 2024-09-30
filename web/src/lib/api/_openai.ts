717a64758875d40e33c02a469d9d1a66a09b5808
Anthony Nowell <anowell@gmail.com>
2024-09-30 12:14:00 -0700

    Quick hacks to appease coolify temporarily

--- web/src/lib/api/_openai.ts
+++ web/src/lib/api/_openai.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-	baseURL: import.meta.env.VITE_API_URL + '/v1',
+	baseURL: (import.meta.env.VITE_API_URL ?? window.location.origin) + '/v1',
 	apiKey: '',
 	dangerouslyAllowBrowser: true
 });
